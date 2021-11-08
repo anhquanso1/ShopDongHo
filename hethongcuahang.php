0fc7e87713315e2a4c60a729726bd5a08b41f2e0
Nguyen Anh Quan <anhquanit1502@gmail.com>
2021-11-08 18:09:47 +0700

    ShopDongHo

--- hethongcuahang.php
+++ hethongcuahang.php
@@ -8,35 +8,46 @@ include 'header.php';
 
 <head>
  <style>
-     .text{
-         padding: 20px 50px;
+     #map{
+         text-align: center;
+     }
+     #text{
+         text-align: center;
      }
 
  </style>
 </head>
+<div id="container">
 <div>
-    <h1>Cơ sở 1</h1>
-    <p>02 võ oanh</p>
-    <div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.0834633205195!2d106.71489441462103!3d10.804919461622072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529ee6c5d3563%3A0xbe1be1c17b50b3f3!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBHaWFvIHRow7RuZyBW4bqtbiB04bqjaSBUUCBIQ00!5e0!3m2!1svi!2s!4v1636367208446!5m2!1svi!2s" width="400" height="300" style="border-radius:20px;" allowfullscreen="" loading="lazy"></iframe>
+    <div id="text" >
+    <h1>Showroom 1</h1>
+    <p>02 Võ Oanh, phường 25, Bình Thạnh , Tp Hồ Chí Minh</p>   
+    </div>
+    <div id="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.0834633205195!2d106.71489441462103!3d10.804919461622072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529ee6c5d3563%3A0xbe1be1c17b50b3f3!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBHaWFvIHRow7RuZyBW4bqtbiB04bqjaSBUUCBIQ00!5e0!3m2!1svi!2s!4v1636367208446!5m2!1svi!2s" width="500" height="400" style="border-radius:10px;" allowfullscreen="" loading="lazy"></iframe>
     </div>
     
 </div>
 <div>
-    <h1>Cơ sở 2</h1>
-    <p>Trần não</p>
-    <div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.2511380485566!2d106.72963911462107!3d10.792067461856286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317526068f53b349%3A0x8e1091a78fa2c77b!2zQ8ahIHPhu58gMiAtIEvDvSB0w7pjIHjDoSBUcsaw4budbmcgxJDhuqFpIGjhu41jIEdpYW8gdGjDtG5nIHbhuq1uIHThuqNpIFRwLkjhu5MgQ2jDrSBNaW5o!5e0!3m2!1svi!2s!4v1636367357160!5m2!1svi!2s" width="400" height="300" style="border-radius:20px;" allowfullscreen="" loading="lazy"></iframe>
+    <div id="text">
+        <h1>Showroom 2</h1>
+    <p>Số 10, đường số 12 Trần Não, Phường Bình An,Quận 2 , TP Hồ Chí Minh </p>
+    </div>
+    <div id="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.2511380485566!2d106.72963911462107!3d10.792067461856286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317526068f53b349%3A0x8e1091a78fa2c77b!2zQ8ahIHPhu58gMiAtIEvDvSB0w7pjIHjDoSBUcsaw4budbmcgxJDhuqFpIGjhu41jIEdpYW8gdGjDtG5nIHbhuq1uIHThuqNpIFRwLkjhu5MgQ2jDrSBNaW5o!5e0!3m2!1svi!2s!4v1636367357160!5m2!1svi!2s" width="500" height="400" style="border-radius:10px;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+    
+</div>
+    <div id="text">
+    <h1>Showroom 3</h1>
+    <p>70 Tô Ký, Tân Chánh Hiệp, Quận 12, TP Hồ Chí Minh</p>
+    </div>
+    <div id="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.294134416075!2d106.61413841462138!3d10.86521846051992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752a1a124f4105%3A0xb89315414737dcb1!2zxJDhuqFpIEjhu41jIFRow7RuZyBW4bqtbiBU4bqjaSBUUCBIQ00gLSBDxqEgc-G7nyAz!5e0!3m2!1svi!2s!4v1636367380760!5m2!1svi!2s" width="500" height="400" style="border-radius:10px;" allowfullscreen="" loading="lazy"></iframe>
     </div>
     
 </div>
 
-    <div>
-    <h1>Cơ sở 3</h1>
-    <p>Tô Ký</p>
-    <div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.294134416075!2d106.61413841462138!3d10.86521846051992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752a1a124f4105%3A0xb89315414737dcb1!2zxJDhuqFpIEjhu41jIFRow7RuZyBW4bqtbiBU4bqjaSBUUCBIQ00gLSBDxqEgc-G7nyAz!5e0!3m2!1svi!2s!4v1636367380760!5m2!1svi!2s" width="400" height="300" style="border-radius:20px;" allowfullscreen="" loading="lazy"></iframe>
-    </div>
-    
 </div>
+
 <?php include 'footer.php';?>

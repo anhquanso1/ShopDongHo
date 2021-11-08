7810be38c8f00b94fcba18dbf4c43561b25927be
Nguyen Anh Quan <anhquanit1502@gmail.com>
2021-11-08 17:36:16 +0700

    ShopDongHo

--- hethongcuahang.php
+++ hethongcuahang.php
@@ -18,7 +18,7 @@ include 'header.php';
     <h1>Cơ sở 1</h1>
     <p>02 võ oanh</p>
     <div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.0834633205195!2d106.71489441462103!3d10.804919461622072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529ee6c5d3563%3A0xbe1be1c17b50b3f3!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBHaWFvIHRow7RuZyBW4bqtbiB04bqjaSBUUCBIQ00!5e0!3m2!1svi!2s!4v1636367208446!5m2!1svi!2s" width="600" height="450" style="border-radius:20px;" allowfullscreen="" loading="lazy"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.0834633205195!2d106.71489441462103!3d10.804919461622072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529ee6c5d3563%3A0xbe1be1c17b50b3f3!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBHaWFvIHRow7RuZyBW4bqtbiB04bqjaSBUUCBIQ00!5e0!3m2!1svi!2s!4v1636367208446!5m2!1svi!2s" width="400" height="300" style="border-radius:20px;" allowfullscreen="" loading="lazy"></iframe>
     </div>
     
 </div>
@@ -26,16 +26,16 @@ include 'header.php';
     <h1>Cơ sở 2</h1>
     <p>Trần não</p>
     <div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.2511380485566!2d106.72963911462107!3d10.792067461856286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317526068f53b349%3A0x8e1091a78fa2c77b!2zQ8ahIHPhu58gMiAtIEvDvSB0w7pjIHjDoSBUcsaw4budbmcgxJDhuqFpIGjhu41jIEdpYW8gdGjDtG5nIHbhuq1uIHThuqNpIFRwLkjhu5MgQ2jDrSBNaW5o!5e0!3m2!1svi!2s!4v1636367357160!5m2!1svi!2s" width="600" height="450" style="border-radius:20px;" allowfullscreen="" loading="lazy"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.2511380485566!2d106.72963911462107!3d10.792067461856286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317526068f53b349%3A0x8e1091a78fa2c77b!2zQ8ahIHPhu58gMiAtIEvDvSB0w7pjIHjDoSBUcsaw4budbmcgxJDhuqFpIGjhu41jIEdpYW8gdGjDtG5nIHbhuq1uIHThuqNpIFRwLkjhu5MgQ2jDrSBNaW5o!5e0!3m2!1svi!2s!4v1636367357160!5m2!1svi!2s" width="400" height="300" style="border-radius:20px;" allowfullscreen="" loading="lazy"></iframe>
     </div>
     
 </div>
 
     <div>
-    <h1>Cơ sở 2</h1>
-    <p>Trần não</p>
+    <h1>Cơ sở 3</h1>
+    <p>Tô Ký</p>
     <div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.294134416075!2d106.61413841462138!3d10.86521846051992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752a1a124f4105%3A0xb89315414737dcb1!2zxJDhuqFpIEjhu41jIFRow7RuZyBW4bqtbiBU4bqjaSBUUCBIQ00gLSBDxqEgc-G7nyAz!5e0!3m2!1svi!2s!4v1636367380760!5m2!1svi!2s" width="600" height="450" style="border-radius:20px;" allowfullscreen="" loading="lazy"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.294134416075!2d106.61413841462138!3d10.86521846051992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752a1a124f4105%3A0xb89315414737dcb1!2zxJDhuqFpIEjhu41jIFRow7RuZyBW4bqtbiBU4bqjaSBUUCBIQ00gLSBDxqEgc-G7nyAz!5e0!3m2!1svi!2s!4v1636367380760!5m2!1svi!2s" width="400" height="300" style="border-radius:20px;" allowfullscreen="" loading="lazy"></iframe>
     </div>
     
 </div>
